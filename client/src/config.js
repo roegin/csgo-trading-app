0ab7c4df7fb4ced8884eb6fcc6ab57d5e2e2f611
roegin <roe.gin@qq.com>
2024-02-27 22:52:30 +0800

    测试

--- client/src/config.js
+++ client/src/config.js
@@ -1,9 +1,9 @@
 let SERVER_URL;
 
-if (process.env.NODE_ENV === 'development') {
- SERVER_URL = process.env.REACT_APP_DEV_SERVER_URL || "http://localhost:4000";
-} else if (process.env.NODE_ENV === 'production') {
- SERVER_URL = process.env.REACT_APP_PROD_SERVER_URL || "http://alex.shinestu.com:4000";
+if (process.env.BUff_ENV != 'development') {
+ SERVER_URL = process.env.REACT_APP_DEV_SERVER_URL || "http://alex.shinestu.com:4000";
+} else if (process.env.BUff_ENV === 'development') {
+ SERVER_URL = process.env.REACT_APP_PROD_SERVER_URL || "http://localhost:4000";
 }
 
 console.log('SERVER_URL',SERVER_URL,process.env.NODE_ENV)
