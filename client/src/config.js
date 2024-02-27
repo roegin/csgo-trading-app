277497c745495bf259a4069a30873ddea1ad4303
roegin <roe.gin@qq.com>
2024-02-27 22:53:31 +0800

    测试

--- client/src/config.js
+++ client/src/config.js
@@ -6,6 +6,6 @@ if (process.env.BUff_ENV != 'development') {
  SERVER_URL = process.env.REACT_APP_PROD_SERVER_URL || "http://localhost:4000";
 }
 
-console.log('SERVER_URL',SERVER_URL,process.env.NODE_ENV)
+console.log('SERVER_URL',SERVER_URL,process.env.BUff_ENV)
 
 export { SERVER_URL };
