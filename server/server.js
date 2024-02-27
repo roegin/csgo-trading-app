669d9058229ac4b3a59c402ba80ae200183e9064
roegin <roe.gin@qq.com>
2024-02-28 01:15:00 +0800

    测试

--- server/server.js
+++ server/server.js
@@ -17,11 +17,7 @@ const MONGOURI = "mongodb+srv://roegin:tideland@alexmongodb.wfchfom.mongodb.net/
 
 
 
-app.use(cors({
- origin: '*',
- methods: ['GET', 'POST'],
- allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
-}));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
