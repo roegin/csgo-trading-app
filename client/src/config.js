4822976761010f92ffd4de2247defbaa13c7853f
roegin <roe.gin@qq.com>
2024-02-28 00:05:19 +0800

    测试

--- client/src/config.js
+++ client/src/config.js
@@ -1,11 +1,13 @@
 let SERVER_URL;
 
-if (process.env.BUff_ENV != 'development') {
- SERVER_URL = process.env.REACT_APP_DEV_SERVER_URL || "http://alex.shinestu.com:4000";
-} else if (process.env.BUff_ENV === 'development') {
- SERVER_URL = process.env.REACT_APP_PROD_SERVER_URL || "http://localhost:4000";
+
+
+if (process.env.BUff_ENV === 'development') {
+    SERVER_URL = process.env.REACT_APP_PROD_SERVER_URL || "http://localhost:4000";
+}else{
+    SERVER_URL = process.env.REACT_APP_DEV_SERVER_URL || "http://alex.shinestu.com:4000";
 }
 
 console.log('SERVER_URL',SERVER_URL,process.env.BUff_ENV)
 
-export { SERVER_URL };
+export { SERVER_URL }; //一到两周上连,四月上旬能开出武器 //厚黑学 /资治通鉴
