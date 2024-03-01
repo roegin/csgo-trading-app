3cc4736debabdd31e728b6a0124c2b3987b10bcf
roegin <roe.gin@qq.com>
2024-03-02 01:35:17 +0800

    测试

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -29,7 +29,7 @@ function logUserMiddleware(req, res, next) {
         // 检查用户是否已有对应的 Currency 文档
         if (!user.currency) {
           // 如果没有，创建一个新的 Currency 文档并保存
-          const newCurrency = new Currency({ value: 0 });
+          const newCurrency = new Currency({ value: 0, name: 'usd' });
           await newCurrency.save();
   
           // 将新创建的 Currency 文档的 _id 赋值给 user 的 currency 字段
