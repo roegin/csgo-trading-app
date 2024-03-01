025ab8406cb3e965be01382a278ca1a49d86f9ef
roegin <roe.gin@qq.com>
2024-03-01 22:30:39 +0800

    测试

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -6,15 +6,22 @@ import { SERVER_URL } from '../config'; // 请根据实际路径调整  //SERVER
 const UserProfile = () => {
  const [user, setUser] = useState(null);
 
- // 获取用户信息的函数
- const getUser = async () => {
- try {
-  const response = await axios.get(SERVER_URL+"/users/profile"); // Assumes user is already logged in
-  setUser(response.data);
- } catch (error) {
-  console.error(error);
- }
- };
+  // 获取用户信息的函数
+  const getUser = async () => {
+    try {
+      // Retrieve the token from sessionStorage
+      const token = sessionStorage.getItem('token');
+
+      // Send the token in the Authorization header
+      const response = await axios.get(SERVER_URL+"/users/profile", {
+        headers: { Authorization: `Bearer ${token}` }
+      });
+
+      setUser(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
  // 当组件挂载时，获取用户信息
  useEffect(() => {
