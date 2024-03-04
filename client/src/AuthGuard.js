f351bfa8d37777b6a43f07476238f5508a09e98e
roegin <roe.gin@qq.com>
2024-03-04 15:02:04 +0800

    测试

--- client/src/AuthGuard.js
+++ client/src/AuthGuard.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'; // 修改这行
 
 export function withAuth(Component) {
   return (props) => {
+    console.log('useNavigate')
     const navigate = useNavigate(); // 更新这行
     const auth_token = sessionStorage.getItem('auth_token');
     console.log('已登陆')
