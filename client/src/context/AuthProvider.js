630fcafb2d9a53508bab3250bac83b62bf42266e
roegin <roe.gin@qq.com>
2024-03-04 15:42:11 +0800

    测试

--- client/src/context/AuthProvider.js
+++ client/src/context/AuthProvider.js
@@ -1,39 +1,45 @@
-// client\src\context\AuthProvider.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AuthContext from "./AuthContext";
 import axios from 'axios';
 
-import { SERVER_URL } from '../config'; // 请根据实际路径调整  //SERVER_URL+'
+import { SERVER_URL } from '../config'; // 请根据实际路径调整
 
 const AuthProvider = ({children}) => {
- const [isAuthenticated, setIsAuthenticated] = useState(
-  sessionStorage.getItem('isAuthenticated') === 'true'
- );
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    sessionStorage.getItem('isAuthenticated') === 'true'
+  );
 
- const login = () => {
- setIsAuthenticated(true);
- };
+  useEffect(() => {
+    const storedIsAuthenticated = sessionStorage.getItem('isAuthenticated') === 'true';
+    if (storedIsAuthenticated !== isAuthenticated) {
+      setIsAuthenticated(storedIsAuthenticated);
+    }
+  }, []);
 
- const logout = () => {
- setIsAuthenticated(false);
- };
+  const login = () => {
+    setIsAuthenticated(true);
+    sessionStorage.setItem('isAuthenticated', 'true');
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    sessionStorage.removeItem('isAuthenticated');
+  };
 
   const registerUser = async (username, password) => {
     try {
-      const res = await axios.post(SERVER_URL+'/users/register', { username, password });
+      const res = await axios.post(`${SERVER_URL}/users/register`, { username, password });
       console.log(res.data);
-      return res
     } catch (err) {
       console.error(err);
     }
   };
 
- return (
- <AuthContext.Provider value={{isAuthenticated, login, logout, registerUser}}>
- {children}
- </AuthContext.Provider>
- );
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout, registerUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
-
