21b7cbfdf408ec0d709105b875f84284377ddbe2
roegin <roe.gin@qq.com>
2024-03-04 16:11:55 +0800

    Revert "测试"
    This reverts commit 630fcafb2d9a53508bab3250bac83b62bf42266e.

--- client/src/context/AuthProvider.js
+++ client/src/context/AuthProvider.js
@@ -1,45 +1,39 @@
-import React, { useState, useEffect } from 'react';
+// client\src\context\AuthProvider.js
+import React, { useState } from 'react';
 import AuthContext from "./AuthContext";
 import axios from 'axios';
 
-import { SERVER_URL } from '../config'; // 请根据实际路径调整
+import { SERVER_URL } from '../config'; // 请根据实际路径调整  //SERVER_URL+'
 
 const AuthProvider = ({children}) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    sessionStorage.getItem('isAuthenticated') === 'true'
-  );
+ const [isAuthenticated, setIsAuthenticated] = useState(
+  sessionStorage.getItem('isAuthenticated') === 'true'
+ );
 
-  useEffect(() => {
-    const storedIsAuthenticated = sessionStorage.getItem('isAuthenticated') === 'true';
-    if (storedIsAuthenticated !== isAuthenticated) {
-      setIsAuthenticated(storedIsAuthenticated);
-    }
-  }, []);
+ const login = () => {
+ setIsAuthenticated(true);
+ };
 
-  const login = () => {
-    setIsAuthenticated(true);
-    sessionStorage.setItem('isAuthenticated', 'true');
-  };
-
-  const logout = () => {
-    setIsAuthenticated(false);
-    sessionStorage.removeItem('isAuthenticated');
-  };
+ const logout = () => {
+ setIsAuthenticated(false);
+ };
 
   const registerUser = async (username, password) => {
     try {
-      const res = await axios.post(`${SERVER_URL}/users/register`, { username, password });
+      const res = await axios.post(SERVER_URL+'/users/register', { username, password });
       console.log(res.data);
+      return res
     } catch (err) {
       console.error(err);
     }
   };
 
-  return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout, registerUser }}>
-      {children}
-    </AuthContext.Provider>
-  );
+ return (
+ <AuthContext.Provider value={{isAuthenticated, login, logout, registerUser}}>
+ {children}
+ </AuthContext.Provider>
+ );
 };
 
 export default AuthProvider;
+
