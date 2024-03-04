5e82ac7a5acf5aed14fbb94f79b9dde815386d71
roegin <roe.gin@qq.com>
2024-03-04 16:51:14 +0800

    测试

--- client/src/context/AuthProvider.js
+++ client/src/context/AuthProvider.js
@@ -1,39 +1,39 @@
-// client\src\context\AuthProvider.js
-import React, { useState } from 'react';
-import AuthContext from "./AuthContext";
+import React, { createContext, useState } from 'react';
 import axios from 'axios';
 
-import { SERVER_URL } from '../config'; // 请根据实际路径调整  //SERVER_URL+'
+import { SERVER_URL } from '../config';
 
-const AuthProvider = ({children}) => {
- const [isAuthenticated, setIsAuthenticated] = useState(
-  sessionStorage.getItem('isAuthenticated') === 'true'
- );
+export const AuthContext = createContext();
 
- const login = () => {
- setIsAuthenticated(true);
- };
-
- const logout = () => {
- setIsAuthenticated(false);
- };
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(sessionStorage.getItem('isAuthenticated') === 'true');
 
   const registerUser = async (username, password) => {
     try {
-      const res = await axios.post(SERVER_URL+'/users/register', { username, password });
-      console.log(res.data);
-      return res
+      const response = await axios.post(`${SERVER_URL}/users/register`, { username, password });
+      console.log(response.data);
     } catch (err) {
       console.error(err);
     }
   };
 
- return (
- <AuthContext.Provider value={{isAuthenticated, login, logout, registerUser}}>
- {children}
- </AuthContext.Provider>
- );
+  const login = () => {
+    setIsAuthenticated(true);
+    sessionStorage.setItem('isAuthenticated', 'true');
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    sessionStorage.setItem('isAuthenticated', 'false');
+    // 清除其他相关sessionStorage项，比如auth-token等
+    sessionStorage.removeItem('auth_token');
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout, registerUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
-export default AuthProvider;
-
+export default AuthContext;
