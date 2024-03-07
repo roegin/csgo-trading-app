18f7dc5b69c3b392bb935e8b483f22b1a033eb2e
roegin <roe.gin@qq.com>
2024-03-08 01:59:31 +0800

    测试twi

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/globals.css'; // 引入全局样式
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
