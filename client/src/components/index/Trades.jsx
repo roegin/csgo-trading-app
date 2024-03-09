b42e7b0b1f77f88461433a99e4f2af1131889457
roegin <roe.gin@qq.com>
2024-03-09 21:46:28 +0800

    OK

--- client/src/components/index/Trades.jsx
+++ client/src/components/index/Trades.jsx
@@ -2,45 +2,48 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import {convertWear} from "../../utilities/Utilities";
 import '../../styles/components/Trades.css';
+import '../../styles/globals.css';
 
-export default function Trades({data}) {
+import { CardContent, Card } from "@/components/ui/card";
 
+export default function Trades({ data }) {
     const gridItems = data.map((trade, index) => (
-        <div key={trade._id} className="trade-grid-item">
-
-            <div className="trade-list">
-                <div><b>[H]</b>ave</div>
-                <ul>
-                    {trade.have.map((item, itemIndex) => (
-                        <li key={itemIndex}>
-                            {item.knife} | {item.finish} ({convertWear(item.wear)})
-                        </li>
-                    ))}
-                </ul>
-
-                <div><b>[W]</b>ant</div>
-                <ul>
-                    {trade.want.map((item, itemIndex) => (
-                        <li key={itemIndex}>
-                            {item.knife} | {item.finish} ({convertWear(item.wear)})
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            {
-                (trade.status === "active") ?
-                    (
-                        <NavLink to={`/offers/${trade._id}`} key={trade._id} className="navlink-offer-btn">
-                            <div className="offerBtn">Make Offer</div>
-                        </NavLink>
-                    ) :
-                    (<div className="completed">Completed</div>)
-            }
-        </div>
+        <Card key={index} className="mb-4 flex flex-col">
+            <CardContent className="flex flex-col justify-between h-full">
+                <div>
+                    <div className="flex flex-row items-center mb-4">
+                        <img src="/path/to/your/item-icon.png" alt="Item Icon" className="w-10 h-10 mr-4" />
+                        <div>
+                            <div className="flex flex-col mb-2">
+                                <span className="text-lg font-semibold">Have</span>
+                                {trade.have.map((item, itemIndex) => (
+                                    <span key={itemIndex}>{item.knife} | {item.finish} ({convertWear(item.wear)})</span>
+                                ))}
+                            </div>
+                            <div className="flex flex-col">
+                                <span className="text-lg font-semibold">Want</span>
+                                {trade.want.map((item, itemIndex) => (
+                                    <span key={itemIndex}>{item.knife} | {item.finish} ({convertWear(item.wear)})</span>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-center mt-4">
+                    {trade.status === "active" ? (
+                        <button className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                            Make Offer
+                        </button>
+                    ) : (
+                        <span className="text-green-500">Completed</span>
+                    )}
+                </div>
+            </CardContent>
+        </Card>
     ));
 
     return (
-        <div className="trade-grid-container">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {gridItems}
         </div>
     );
